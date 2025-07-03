d960714c8f9bdaed95920efadddf3e775d2f7844
Sir-Deon <sird655@gmail.com>
2025-07-03 17:58:45 +0100

    Finished setting up the ui, setup redux, router, tailwindcss, icons and loaders

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 function App() {
   const showAlert = () => {
     console.log(window.electron.getCountries());
@@ -14,14 +12,14 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <h1>Welcome to TPN GUI</h1>
-        <p>This is a simple Electron application.</p>
-        <button onClick={showAlert}>Show Alert</button>
-        <button onClick={showConfirm}>Show Confirm</button>
+    <div className="w-full h-[100vh] flex items-center justify-center bg-white text-black">
+      <div className="text-center">
+        <h1 className="text-6xl mb-5">Welcome to TPN GUI</h1>
+        <p className="mb-4">This is a simple Electron application.</p>
+        <button className="bg-[#1a1a1a] cursor-pointer rounded p-2 mr-2 text-white mt-2"  onClick={showAlert}>Show Alert</button>
+        <button className="bg-[#1a1a1a] cursor-pointer rounded p-2 mr-2 text-white mt-2" onClick={showConfirm}>Show Confirm</button>
       </div>
-    </>
+    </div>
   );
 }
 
