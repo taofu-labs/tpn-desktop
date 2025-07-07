603f86f4bd523c852e417928829320893aafd731
marvikomo <marvikomo@gmail.com>
2025-07-07 09:04:29 +0100

    added dev tools for dev env

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -28,6 +28,7 @@ app.whenReady().then(async () => {
   });
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
