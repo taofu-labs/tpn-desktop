68c15b58d6384a3e08f5ccf47978d0fef4d97cc0
marvikomo <marvikomo@gmail.com>
2025-07-07 16:38:19 +0100

    implemented tpn service and ipc handlers

--- src/electron/ipcHandlers.ts
+++ src/electron/ipcHandlers.ts
@@ -1,7 +1,19 @@
 import { ipcMain, shell } from "electron"
+import { ConnectionInfo, StatusInfo } from "./tpn-cli.js"
 
-// export function initializeIpcHandlers(deps: IIpcHandlerDeps): void {
-// ipcMain.handle("set-initial-credits", async (_event, credits: number) => {
 
-// })
-//}
+export interface IpcServices {
+  tpnService: {
+    getCountries(): Promise<string[]>
+    connect(country?: string): Promise<ConnectionInfo>
+    checkStatus(): Promise<StatusInfo>
+    disconnect(): Promise<any> // or whatever disconnect returns
+  }
+
+}
+
+export function initializeIpcHandlers(services: IpcServices): void {
+ipcMain.handle("getCountries", async () => {
+  return await  services.tpnService.getCountries();
+})
+}
