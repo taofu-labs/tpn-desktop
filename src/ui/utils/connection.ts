91d4d7aa429547f807fb76affdcb9bb56126ab83
Sir-Deon <sird655@gmail.com>
2025-11-12 14:38:32 +0100

    initial comit

--- src/ui/utils/connection.ts
+++ src/ui/utils/connection.ts
@@ -1,6 +1,6 @@
 export const leaseDurations = [
-    { time: "10 Minutes", minute: 10 },
-    { time: "20 Minutes", minute: 20 },
-    { time: "1 Hour", minute: 60 },
+    { time: "10 Minutes", minute: 600 },
+    { time: "20 Minutes", minute: 1200 },
+    { time: "1 Hour", minute: 3600 },
   ];
   
