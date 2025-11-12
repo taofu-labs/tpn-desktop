e630d5c62ed650a9acbbb9410efd60183c4007dc
Sir-Deon <sird655@gmail.com>
2025-11-12 15:19:16 +0100

    fixed connection issue

--- src/ui/utils/connection.ts
+++ src/ui/utils/connection.ts
@@ -1,12 +1,6 @@
 export const leaseDurations = [
-<<<<<<< HEAD
-    { time: "10 Minutes", minute: 600 },
-    { time: "20 Minutes", minute: 1200 },
-    { time: "1 Hour", minute: 3600 },
-=======
     { time: "10 Minutes", minute: 10 },
     { time: "20 Minutes", minute: 20 },
     { time: "1 Hour", minute: 60 },
->>>>>>> 67d21fc3fb7ff7bf0a819ec11e354a58ff11c306
   ];
   
